01e660ae0cc86071a82f56674c6bd5d1b1af471c
Kelvis Alvarez <kelvis.alvarez20@gmail.com>
2026-03-17 20:43:59 -0300

    corrección de error de lectura

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Solo cachea el shell de la app (HTML, fuentes).
 // Los datos de la colección siempre vienen de Firebase (nunca se cachean aquí).
 
-const CACHE_NAME = 'pokedex-v38';
+const CACHE_NAME = 'pokedex-v39';
 const BASE = '/pokecollector';
 
 const SHELL = [
