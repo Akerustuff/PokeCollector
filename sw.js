57cd3f42ce29ee108db807f2e9bbfc14da21373f
Kelvis Alvarez <kelvis.alvarez20@gmail.com>
2026-03-17 16:19:30 -0300

    Correción de busqueda de serebii

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Solo cachea el shell de la app (HTML, fuentes).
 // Los datos de la colección siempre vienen de Firebase (nunca se cachean aquí).
 
-const CACHE_NAME = 'pokedex-v21';
+const CACHE_NAME = 'pokedex-v22';
 const BASE = '/pokecollector';
 
 const SHELL = [
