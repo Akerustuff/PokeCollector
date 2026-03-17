58a9a2fd155a5bbe65a535ab39e2ff780968f28a
Kelvis Alvarez <kelvis.alvarez20@gmail.com>
2026-03-17 18:04:25 -0300

    actualización de la persistencia

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Solo cachea el shell de la app (HTML, fuentes).
 // Los datos de la colección siempre vienen de Firebase (nunca se cachean aquí).
 
-const CACHE_NAME = 'pokedex-v29';
+const CACHE_NAME = 'pokedex-v31';
 const BASE = '/pokecollector';
 
 const SHELL = [
