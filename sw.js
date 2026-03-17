1571c8d38c7816f3ec53dafcec3ed5693feb0cf3
Kelvis Alvarez <kelvis.alvarez20@gmail.com>
2026-03-17 01:03:46 -0300

    actualizacion de sprites

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Solo cachea el shell de la app (HTML, fuentes).
 // Los datos de la colección siempre vienen de Firebase (nunca se cachean aquí).
 
-const CACHE_NAME = 'pokedex-v12';
+const CACHE_NAME = 'pokedex-v13';
 const BASE = '/pokecollector';
 
 const SHELL = [
